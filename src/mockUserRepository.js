d18bfb8082f6fb57d4a8d2c4d15f2e9d87cf35cd
Martin Hagen <martin@partins.no>
2023-05-05 16:54:07 +0200

    Added generations of users

--- src/mockUserRepository.js
+++ src/mockUserRepository.js
@@ -1,14 +1,24 @@
 import { User } from "./user.js";
+import { adjectives, colors, animals, starWars, languages, uniqueNamesGenerator, NumberDictionary } from "unique-names-generator";
+import randomstring from "randomstring"
 
 const NUM_OF_FRIENDS = 1
+const NUM_OF_USERS = 100
+const PASSWORD_LENGTH = 4
 
-const userRepo = [
-    new User(1, "test1", "pass"),
-    new User(2, "test2", "pass"),
-    new User(3, "test3", "pass"),
-    new User(4, "test4", "pass"),
-]
+const userRepo = []
 
+const numDic = NumberDictionary.generate({ min: 1, max: 1000 })
+const genUsers = () => {
+    for (let i = 0; i < NUM_OF_USERS; i++) {
+        // Unique name generation taken from: 
+        // https://www.npmjs.com/package/unique-names-generator (05.05.23)
+        userRepo.push(new User(i + 1, uniqueNamesGenerator({
+            dictionaries: [adjectives, colors, animals, starWars, languages, numDic],
+            length: 6
+        }), randomstring.generate(PASSWORD_LENGTH)))
+    }
+}
 /**
  * Generates NUM_OF_FRIENDS of friends per user pseudorandomly
  */
@@ -24,6 +34,8 @@ const genFriendships = () => {
     }
 }
 
-genFriendships()
+genUsers();
+genFriendships();
+
 
 export { userRepo }
